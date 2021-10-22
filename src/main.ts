0a57a95cc3ebbe885dd3494edf528696c3f209f4
deanxx <deanxx999@gmail.com>
2021-10-22 12:37:17 +0900

    add middleware and cors option on main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: 'access_token',
+  });
+  app.use(json({ limit: '100mb' }));
+  app.use(urlencoded({ extended: true, limit: '100mb' }));
   await app.listen(3003);
 }
 bootstrap();
